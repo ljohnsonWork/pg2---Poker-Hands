dc5bf243e4d415a7e75a73e038574864e1c4e240
Lawrence Johnson <lawrencejohnson@rvr3517-117.resapt.nmu.edu>
2014-09-20 18:46:29 -0400

    Initial Commit

---
+++ pg2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pg2
+//
+//  Created by Lawrence Johnson on 9/20/14.
+//  Copyright (c) 2014 Lawrence Johnson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
